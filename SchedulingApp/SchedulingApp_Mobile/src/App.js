bd34ec99d0b9ead2ef7765e821b4f34b3afcc0d7
NadoCo-Nathan <nadocouniversal@gmail.com>
2021-07-22 06:09:06 +1000

    Project Setup
    + Added react native paper
    + Added react native paper vector icons

--- SchedulingApp/SchedulingApp_Mobile/src/App.js
+++ SchedulingApp/SchedulingApp_Mobile/src/App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+} from "react-native"; /* No HTML elements in React native. Must use built-in components*/
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* View is like a div */}
       <Text>Open up App.js to start working on your app!</Text>
+      <Button mode="contained">A Sexy Button</Button>
       <StatusBar style="auto" />
-    </View>
+    </View> /* View maps to whatever platform. UIView = ios. AndroidView= android */
   );
 }
 
 const styles = StyleSheet.create({
+  /* Similar to makeStyles */
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
