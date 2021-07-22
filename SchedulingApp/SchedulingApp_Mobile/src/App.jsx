14903685791c67a88884db5c9bdde72ae5974939
NadoCo-Nathan <nadocouniversal@gmail.com>
2021-07-23 06:36:11 +1000

    Created new views
    + Added "Schedule View"
    + Added "Tasks View"
    
    ~ working on tabs integration to switch between views

--- SchedulingApp/SchedulingApp_Mobile/src/App.jsx
+++ SchedulingApp/SchedulingApp_Mobile/src/App.jsx
@@ -2,21 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Button } from "react-native-paper";
 import {
+  SafeAreaView,
+  FlatList,
   StyleSheet,
   Text,
   View,
 } from "react-native"; /* No HTML elements in React native. Must use built-in components*/
+import Layout from "./Layout";
 
-export default function App() {
+export default App = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {/* View is like a div */}
-      <Text>Open up App.js to start working on your app!</Text>
+      <Layout />
+      {/* <Text>Open up App.js to start working on your app!</Text>
       <Button mode="contained">A Sexy Button</Button>
-      <StatusBar style="auto" />
-    </View> /* View maps to whatever platform. UIView = ios. AndroidView= android */
+      <StatusBar style="auto" /> */}
+    </SafeAreaView> /* View maps to whatever platform. UIView = ios. AndroidView= android */
   );
-}
+};
 
 const styles = StyleSheet.create({
   /* Similar to makeStyles */
