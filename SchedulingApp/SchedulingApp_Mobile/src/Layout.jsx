d6ada3c36baabc59a5ef00ec5f1bbfd82778171d
NadoCo-Nathan <nadocouniversal@gmail.com>
2021-07-27 06:24:14 +1000

    Working on tabs

--- SchedulingApp/SchedulingApp_Mobile/src/Layout.jsx
+++ SchedulingApp/SchedulingApp_Mobile/src/Layout.jsx
@@ -1,15 +1,21 @@
+import React from "react";
 import { View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ScheduleView from "./Views/ScheduleView";
 import TasksView from "./Views/TasksView";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default Layout = () => {
+  const Tab = createBottomTabNavigator();
+
   return (
     <View>
-      <Tab.Navigator>
-        <Tab.Screen name="Schedule" component={ScheduleView} />
-        <Tab.Screen name="Tasks" component={TasksView} />
-      </Tab.Navigator>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Schedule" component={ScheduleView} />
+          <Tab.Screen name="Tasks" component={TasksView} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 };
---
+++ SchedulingApp/SchedulingApp_Mobile/src/Views/TasksView.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default TasksView = () => {
+  return (
+    <View>
+      <Text>Tasks View</Text>
+    </View>
+  );
+};
