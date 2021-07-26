55acad9671cf455ea66e06a493b2dad7ca815cb9
NadoCo-Nathan <nadocouniversal@gmail.com>
2021-07-27 05:49:42 +1000

    Update Layout - Add Tabs

--- SchedulingApp/SchedulingApp_Mobile/src/Layout.jsx
+++ SchedulingApp/SchedulingApp_Mobile/src/Layout.jsx
@@ -1,5 +1,15 @@
 import { View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ScheduleView from "./Views/ScheduleView";
+import TasksView from "./Views/TasksView";
 
 export default Layout = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Tab.Navigator>
+        <Tab.Screen name="Schedule" component={ScheduleView} />
+        <Tab.Screen name="Tasks" component={TasksView} />
+      </Tab.Navigator>
+    </View>
+  );
 };
