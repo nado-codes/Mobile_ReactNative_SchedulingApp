1094e1d5d163950b7833e7c9f1f6dc5148a97f20
NadoCo-Nathan <nadocouniversal@gmail.com>
2021-08-10 05:57:19 +1000

    Frontend
    + [ScheduleView] Installed package "react-native-draggable-flatlist"
    - [ScheduleView] Removed ScrollView and replaced with "draggable flat list"
    + [ScheduleView] Added drag-and-drop to scroll list

--- SchedulingApp/SchedulingApp_Mobile/src/Views/ScheduleView.jsx
+++ SchedulingApp/SchedulingApp_Mobile/src/Views/ScheduleView.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import React, { useCallback, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import DraggableFlatList from "react-native-draggable-flatlist";
 
 export default ScheduleView = (/* {list} */) => {
   // Note: .. DUMMY DATA
@@ -10,14 +17,96 @@ export default ScheduleView = (/* {list} */) => {
   };
 
   const itemCount = Array.from(Array(20).keys());
-  const list = itemCount.map((n) => ({
+  const listOrigin = itemCount.map((n) => ({
     time: makeTime(n),
     task: "Do a thing",
   }));
 
+  // REACT DRAGGABLE CODE
+  const NUM_ITEMS = 10;
+
+  function getColor(i) {
+    const multiplier = 255 / (NUM_ITEMS - 1);
+    const colorVal = i * multiplier;
+    return `rgb(${colorVal}, ${Math.abs(128 - colorVal)}, ${255 - colorVal})`;
+  }
+
+  const exampleData = [...Array(20)].map((d, index) => {
+    const backgroundColor = getColor(index);
+    return {
+      key: `item-${backgroundColor}`,
+      label: String(index),
+      backgroundColor,
+    };
+  });
+
+  /* const renderItem = useCallback(({ item, index, drag, isActive }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          height: 100,
+          backgroundColor: isActive ? "red" : "green",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onLongPress={drag}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 32,
+          }}
+        >
+          {item[Object.keys(item)[1]]}
+        </Text>
+      </TouchableOpacity>
+    );
+  }, []); */
+
+  // END REACT DRAGGABLE CODE
+
+  const [data, setData] = useState(listOrigin);
+
+  const renderItem = useCallback(
+    ({ item, index, drag, isActive }) => (
+      <TouchableOpacity
+        key={index}
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          height: 50,
+        }}
+        onLongPress={drag}
+      >
+        {isActive && (
+          <View
+            style={{
+              position: "absolute",
+              zIndex: 100,
+              width: "100%",
+              height: 50,
+              backgroundColor: "white",
+              opacity: 0.5,
+            }}
+          />
+        )}
+        <View style={{ ...styles.cell, flex: 0.5, backgroundColor: "white" }}>
+          <Text style={{ color: "#000" }}>{item?.time ?? `NO_TIME`}</Text>
+        </View>
+        <View style={{ ...styles.cell, flex: 1 }}>
+          <Text>{item?.task ?? `NO_TASK`}</Text>
+        </View>
+      </TouchableOpacity>
+    ),
+    []
+  );
+
   return (
     <View style={{ display: "flex", flex: 1 }}>
-      <ScrollView nestedScrollEnabled={true}>
+      {/* <ScrollView nestedScrollEnabled={true}>
         {list.map((l, i) => (
           <View
             key={i}
@@ -39,7 +128,13 @@ export default ScheduleView = (/* {list} */) => {
             </View>
           </View>
         ))}
-      </ScrollView>
+          </ScrollView> */}
+      <DraggableFlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => `${data.indexOf(item)}`} //`draggable-item-${item.key}`}
+        onDragEnd={({ data }) => setData(data)}
+      />
     </View>
   );
 };
